97c912b3915632bea8f070660056540e2f434475
Pocehome <74858692+Pocehome@users.noreply.github.com>
2024-10-17 18:36:11 +0300

    without "true" solution

--- theta_neur_lab.py
+++ theta_neur_lab.py
@@ -26,15 +26,16 @@ def theta_neur_dyn(gamma, n):
 #     return normalized
 
 
-def draw_graph(theta_num, theta_true, theta_lim, t, limits, title): 
+def draw_graph(theta_num, theta_true, t, limits, title, theta_lim=0): 
     x_lims, t_lims = limits
     plt.ylim(x_lims[0], x_lims[1])
     plt.xlim(t_lims[0], t_lims[1])
         
-    for el in theta_num:
-        plt.plot(t, el, 'darkgreen', label='num. sol.')
+    plt.plot(t, theta_num, 'darkgreen', label='num. sol.')
     # plt.plot(t, theta_true, 'red', label='true sol.')
-    plt.axhline(y=theta_lim, color='black', linestyle='--', label='\u03B81')
+    
+    if theta_lim:
+        plt.axhline(y=theta_lim, color='black', linestyle='--', label='\u03B81')
     
     plt.title(f"\u03B3={title[0]}, n={title[1]}")
     plt.xlabel('time', fontsize=10, color='black')
@@ -45,37 +46,42 @@ def draw_graph(theta_num, theta_true, theta_lim, t, limits, title):
     plt.show()
     
     
-def make_graph(gamma, step_n):
+def make_graph(gamma, step_count):
     # initial values
     theta0 = 0.
     
-    # numerical integration
     for n in [1, 3]:
+        # numerical integration
         rhs = theta_neur_dyn(gamma, n)
         sol = theta0
         
         if n == 3 and 1 < gamma < 1.2:
-            step_n *= 2
+            step_count *= 2
             
         step_size = 0.01
-        arr_sol = np.array([0.] * step_n)
-        arr_t = np.array([0.] * step_n)
+        arr_sol = np.array([0.] * step_count)
+        arr_t = np.array([0.] * step_count)
         
-        for k in range(step_n):
+        for k in range(step_count):
             sol = RK4_step(rhs, sol, step_size)
             arr_sol[k] = sol
             arr_t[k] = k * step_size
-            
-        theta_lim = -1
-        if gamma < 1:
-            theta_lim = 2*np.pi - n * np.acos(gamma)
-            norm_arr_sol = [np.mod(arr_sol, 2*np.pi)]
-        else:
-            norm_arr_sol = [np.mod(arr_sol, 2*np.pi)]
+
+        theta_true = [0]    # temporary zaglushka
         
-        theta_true = [0]
-        draw_graph(norm_arr_sol, theta_true, theta_lim, arr_t,
-                   [(0, 2*np.pi), (0, step_n*step_size)], [gamma, n])
+        # draw graphs
+        norm_arr_sol = np.mod(arr_sol, 2*np.pi)     # [0; 2pi]
+        
+        if gamma < 1:
+            # take neg solution of equation and reduce it to [0, 2pi]:
+            theta_lim = 2*np.pi - n * np.acos(gamma)
+            
+            draw_graph(norm_arr_sol, theta_true, arr_t,
+                       [(0, 2*np.pi), (0, step_count*step_size)], [gamma, n],
+                       theta_lim=theta_lim)
+        else:
+            draw_graph(norm_arr_sol, theta_true, arr_t,
+                       [(0, 2*np.pi), (0, step_count*step_size)], [gamma, n])
 
 
 if __name__ == '__main__':
